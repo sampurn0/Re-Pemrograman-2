4129f40e9cc876bafd62ad0d194fb3d2f6ccea5e
fitri15 <f.ya15@yahoo.co.id>
2014-03-07 15:40:34 +0700

    praktikum 1

---
+++ praktikum2-1/src/main/java/latihan/praktikum2/time.java
@@ -0,0 +1,12 @@
+package latihan.praktikum2;
+
+import org.joda.time.DateTime;
+
+public class time {
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
+		
+		DateTime dt = new DateTime();
+		System.out.println("Waktu ini : " + dt);
+	}
+}
